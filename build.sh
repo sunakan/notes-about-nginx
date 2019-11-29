3da93fbb77f103d0bbdf084da8d31b31a03ab162
sunakan <sunakan03@gmail.com>
2019-11-29 19:41:13 +0900

    [Add] 日本時間の取得

--- build.sh
+++ build.sh
@@ -5,5 +5,9 @@
 # 弱み：alpha/hogeとbeta/hoge
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 CURRENT_BRANCH=${CURRENT_BRANCH##*/}
-
 echo ${CURRENT_BRANCH}
+
+# (日本)時間の取得
+# 20191231T213015 => 2019年12月31日の21h30m15s
+TZ=JST BUILD_DATE=`date "+%Y%m%dT%H%M%S"`
+echo ${BUILD_DATE}
