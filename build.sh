56515652a1905dab5fe76565282ef646070d1d3c
sunakan <sunakan03@gmail.com>
2019-11-29 19:47:41 +0900

    [Add] gitのコミットIDをとる

--- build.sh
+++ build.sh
@@ -11,3 +11,8 @@ echo ${CURRENT_BRANCH}
 # 20191231T213015 => 2019年12月31日の21h30m15s
 TZ=JST BUILD_DATE=`date "+%Y%m%dT%H%M%S"`
 echo ${BUILD_DATE}
+
+# コミットIDの最初の8文字
+COMMIT_ID=`git show -s --format=%H`
+COMMIT_ID=${COMMIT_ID:0:8}
+echo ${COMMIT_ID}
