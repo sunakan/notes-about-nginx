a17393ea95ccb6110ab345a9684759f6e8a8a95b
sunakan <sunakan03@gmail.com>
2019-11-30 16:43:30 +0900

    更に意地悪して、変数に代入し直したり、少しいじったりする

--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo "====="
+echo "=====HOGE"
 echo ${HOGE}
+echo "====="
+
+TEST_SECRET2=${HOGE}
+echo "=====TEST_SECRET2"
+echo ${TEST_SECRET2}
+echo "====="
+
+TEST_SECRET3=${HOGE:0:-1}
+echo "=====TEST_SECRET3"
+echo ${TEST_SECRET3}
 echo "====="
