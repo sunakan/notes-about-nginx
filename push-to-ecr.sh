619761cd98ca6598f1a5651b1f6103327598b6ea
sunakan <sunakan03@gmail.com>
2019-11-30 15:15:13 +0900

    [Fix] clean

--- push-to-ecr.sh
+++ push-to-ecr.sh
@@ -12,15 +12,15 @@
 # 手動実行するときは、コメントインする
 # $(aws ecr get-login --no-include-email)
 
-# actionsを利用するときは、AWS_ECR_REGISTORYをここで定義不要
+# actionsを利用するときは、AWS_ECR_REGISTRYをここで定義不要
 #
 # account idがaws cliで取ってこれるらしいのでそれを利用
 # AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 # AWS_DEFAULT_REGION=ap-northeast-1
-# AWS_ECR_REGISTORY=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
+# AWS_ECR_REGISTRY=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
 
 # ECRにpushする用のimage tagを付与
-docker tag sunakan/suna-nginx:${IMAGE_TAG} "${AWS_ECR_REGISTORY}/${AWS_ECR_REPO_NAME}:${IMAGE_TAG}"
+docker tag sunakan/suna-nginx:${IMAGE_TAG} "${AWS_ECR_REGISTRY}/${AWS_ECR_REPO_NAME}:${IMAGE_TAG}"
 
 echo "=================="
 docker image ls -a
