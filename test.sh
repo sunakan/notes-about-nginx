0b4b7c5b1a39cf3d2f76f581399142ba979ef654
sunakan <sunakan03@gmail.com>
2019-11-30 16:50:21 +0900

    意地悪

--- test.sh
+++ test.sh
@@ -13,3 +13,18 @@ TEST_SECRET3=${HOGE}HOGEHOGEHOGEHOGEHOGEHOGE
 echo "=====TEST_SECRET3"
 echo ${TEST_SECRET3}
 echo "====="
+
+TEST_SECRET4=${HOGE:0:1}
+echo "=====TEST_SECRET4最初の1文字を出力"
+echo ${TEST_SECRET4}
+echo "====="
+
+TEST_SECRET5=${HOGE:1}
+echo "=====TEST_SECRET5最初の1文字を抜いて出力"
+echo ${TEST_SECRET5}
+echo "====="
+
+TEST_JOIN=${TEST_SECRET4}${TEST_SECRET5}
+echo "=====つなげてみた"
+echo ${TEST_JOIN}
+echo "====="
