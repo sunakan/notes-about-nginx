e99c7b2ef9f63d7d419f8bf8b143908f7bb16fd3
sunakan <sunakan03@gmail.com>
2019-11-30 15:11:31 +0900

    [Add] AWSが作っているActionを利用して、ECRへプッシュ直前まで

--- push-to-ecr.sh
+++ push-to-ecr.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-echo "===========IMAGE_TAG"
-echo ${IMAGE_TAG}
-echo "==========="
+# 外から必要な変数
+#
+# AWS_ECR_REPO_NAME=ベタ書きした時のリスクは。。。？
+# IMAGE_TAG=push-docker-image-to-ecr-20191130T113404-317904d6
 
+# Login
+#
 # loginするためのコマンドをaws ecr get-loginで発行
 # それを即実行するために$()で囲ってる
-$(aws ecr get-login --no-include-email)
+# 手動実行するときは、コメントインする
+# $(aws ecr get-login --no-include-email)
+
+# actionsを利用するときは、AWS_ECR_REGISTORYをここで定義不要
+#
+# account idがaws cliで取ってこれるらしいのでそれを利用
+# AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+# AWS_DEFAULT_REGION=ap-northeast-1
+# AWS_ECR_REGISTORY=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
+
+# ECRにpushする用のimage tagを付与
+docker tag sunakan/suna-nginx:${IMAGE_TAG} "${AWS_ECR_REGISTORY}/${AWS_ECR_REPO_NAME}:${IMAGE_TAG}"
+
+echo "=================="
+docker image ls -a
+echo "=================="
